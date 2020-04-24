969f2c8138b8f4b65a29622b65272cbc0c50003f
KamilJankowski99 <kjankowski@wow.wroclaw.pl>
2020-04-24 23:46:26 +0200

    Dodano ikonę

--- server/index.js
+++ server/index.js
@@ -1,20 +1,17 @@
 const path = require('path')
 const logger = require('koa-logger')
 const Koa = require('koa')
+const favicon = require('koa-favicon')
 const serve = require('koa-static')
 
 const app = new Koa()
+app.use(favicon(path.join(__dirname, '..', 'client', 'rocket.ico')))
 app.use(logger())
 const port = process.env.PORT || 3000
-
 app.use(serve(path.resolve(__dirname, '..', 'client')))
-
 const userRoutes = require('./routes/users')
 app.use(userRoutes.routes())
-
 const taskRoutes = require('./routes/tasks')
 app.use(taskRoutes.routes())
-
 app.listen(port)
-
 console.log('App is listening at http://127.0.0.1:3000')
